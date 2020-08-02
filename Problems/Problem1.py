c5d47b690dfa02b93aca751b2667cd7c5a09be60
Austin <austxnwhxte@gmail.com>
2020-08-02 00:18:29 -0400

    SOLUTION FOUND

--- Problems/Problem1.py
+++ Problems/Problem1.py
@@ -1,51 +1,52 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
+
+#variables for counting mulitples of 3
 a = 3983
 t = 0
 n = 1
 counteri = 0
-for i in range(0, 350):
+while n in range(1, 1000):
 
     k = (3 * n)
     n += 1
-    if k < 1000:
+    if k <= 990:
         t = t + k
         counteri += 1
-        #print(k)
-    #print(t)
 
-
-#print("END OF 3's")
-#print("")
-print(counteri)
+#variables for counting mulitples of 5
+#print(counteri)
 p = 0
 m = 1
 counterj = 0
-for j in range(0, 200):
+while m in range(1, 1000):
 
     l = (5 * m)
     m += 1
-    if l < 1000:
+    if l <= 990:
         p = p + l
         counterj += 1
-        #print(l)
 
-print(counterj)
+
+
+#variables for counting mulitples of 15
+#print(counterj)
 r = 0
 c = 1
 counterz = 0
-for z in range(0, 500):
+
+while c in range(1, 67):
 
     v = (15 * c)
     c += 1
-    if v < 1000:
+    if v <= 990:
         r = r + v
         counterz += 1
         #print(l)
     #print(p)
-print(counterz)
-print("a is:", a)
-print("t is:", t)
-print("p is:", p)
-print("v is:", v)
-print("a + t + p - v is:", (a + t + p - v))
+#print(counterz)
+#print("a is:", a)
+#print("t is:", t)
+#print("p is:", p)
+#print("r is:", r)
+print("a + t + p - v is:", (a + t + p - r))
